88248c479babe63b4ae7fe1c3c74434b3ae7c5e6
jigysha-art <s.jigisha786@gmail.com>
2024-08-23 23:53:50 +0530

    event

--- lib/home/bloc/home_event.dart
+++ lib/home/bloc/home_event.dart
@@ -4,3 +4,7 @@ part of 'home_bloc.dart';
 abstract class HomeEvent {}
 
 class HomeProductWishlistButtonClickedEvent extends HomeEvent {}
+
+class HomeProductCartButtonClickedEvent extends HomeEvent {}
+
+class HomeWishlistButtonNavigateClickedEvent extends HomeEvent {}

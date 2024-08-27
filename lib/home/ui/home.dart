2153b1105cb7aa4f85d2d3ebe95292b50f933b04
jigysha-art <s.jigisha786@gmail.com>
2024-08-27 23:54:19 +0530

    initial

--- lib/home/ui/home.dart
+++ lib/home/ui/home.dart
@@ -14,6 +14,7 @@ class _HomeState extends State<Home> {
       appBar: AppBar(
         backgroundColor: Colors.orange,
         title: Text("Grocery App"),
+        leading: Icon(Icons.insert_emoticon_sharp),
       ),
     );
   }

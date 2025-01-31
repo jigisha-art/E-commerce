81bca89cdc6346052a99a603db7327afab007303
jigisha-art <s.jigisha786@gmail.com>
2025-01-31 18:47:34 +0530

    commontextFieled

--- lib/commonTextFiled.dart
+++ lib/commonTextFiled.dart
@@ -1,21 +1,42 @@
 import 'package:flutter/material.dart';
 
+class CommonTextField extends StatelessWidget {
+  final TextEditingController controller;
+  final String labelText;
+  final String hintText;
+  final TextInputType keyboardType;
+  final bool isPassword;
+  final IconData? prefixIcon;
+  final int maxLines;
+  final Function(String)? onChanged;
 
-class common_textfield extends StatefulWidget {
-  const common_textfield({super.key});
+  const CommonTextField({
+    Key? key,
+    required this.controller,
+    required this.labelText,
+    this.hintText = '',
+    this.keyboardType = TextInputType.text,
+    this.isPassword = false,
+    this.prefixIcon,
+    this.maxLines = 1,
+    this.onChanged,
+  }) : super(key: key);
 
-  @override
-  State<common_textfield> createState() => _common_textfieldState();
-}
-
-class _common_textfieldState extends State<common_textfield> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("App Bar",
-
+    return TextField(
+      controller: controller,
+      obscureText: isPassword, // Hide text if it's a password field
+      keyboardType: keyboardType,
+      maxLines: maxLines,
+      onChanged: onChanged,
+      decoration: InputDecoration(
+        labelText: labelText,
+        hintText: hintText,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8.0),
         ),
+        prefixIcon: prefixIcon != null ? Icon(prefixIcon) : null,
       ),
     );
   }

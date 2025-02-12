be446aa990da6210d3033a7f59ae390b86efec73
jigisha-art <s.jigisha786@gmail.com>
2025-02-12 19:09:18 +0530

    graph card 2

---
+++ lib/home/graphCard 2.dart
@@ -0,0 +1,16 @@
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/material.dart';
+
+class graphCard extends StatefulWidget {
+  const graphCard({super.key});
+
+  @override
+  State<graphCard> createState() => _graphCardState();
+}
+
+class _graphCardState extends State<graphCard> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
